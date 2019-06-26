a5826ed7e1ab2a5e3fc98345c8bd076433047f5a
Caleb Severn <calsev@gmail.com>
2019-06-26 13:53:45 -0800

    Simplified resource interface implementation

--- resources/checksum_helper.rb
+++ resources/checksum_helper.rb
@@ -6,19 +6,15 @@ provides :checksum_file
 default_action :save
 
 property :source_path, String, required: true
-property :target_path, String, required: true
+property :target_path, String, name_property: true
 property :save_path, [true, false], default: true
 property :checksum_algorithm, String, default: 'md5'
 property :include_metadata, [true, false], default: true
 
 action :save do
-  save_checksum_file_helper(@new_resource)
+  save_checksum_file(@new_resource)
 end
 
-action_class.class_eval do
-  include ::ChecksumFile::Helper
-
-  def save_checksum_file_helper(new_resource)
-    save_checksum_file(new_resource)
-  end
+action_class do
+  include ChecksumFile::Helper
 end
