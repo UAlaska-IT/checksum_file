5c1e927181ecf29747629af3627839ec065d8086
Caleb Severn <calsev@gmail.com>
2019-07-17 12:08:55 -0800

    Added tests for checksum files

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 path_to_data_directory = '/tmp/checksum_data'
+path_to_checksum_directory = '/tmp/checksum_checksum'
 path_to_test_directory = '/tmp/checksum_test'
 
 filenames = [
@@ -41,6 +42,15 @@ includes.each do |include|
   algorithms.each do |algorithm|
     paths.each do |path|
       base_name = "#{File.basename(path)}_#{include[0]}_#{include[1]}_#{algorithm}"
+
+      # Check checksum itself
+      describe file File.join(path_to_checksum_directory, base_name) do
+        it { should exist }
+        it { should be_file }
+        it { should be_mode 0o644 }
+        it { should be_owned_by 'root' }
+        it { should be_grouped_into 'root' }
+      end
 
       # Check first creation
       describe file File.join(path_to_test_directory, "#{base_name}_create") do
@@ -119,6 +129,15 @@ end
 includes.each do |include|
   algorithms.each do |algorithm|
     base_name = "#{include[0]}_#{include[1]}_#{algorithm}"
+
+    # Check checksum itself
+    describe file File.join(path_to_checksum_directory, base_name) do
+      it { should exist }
+      it { should be_file }
+      it { should be_mode 0o701 }
+      it { should be_owned_by 'bud' }
+      it { should be_grouped_into 'bud' }
+    end
 
     # Check first creation
     describe file File.join(path_to_test_directory, "#{base_name}_create") do
